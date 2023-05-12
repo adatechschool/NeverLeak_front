f8d5f6f0ef43e35875e0a65ce2818b416b347c1f
pierre merlaud <pmerlaud007@gmail.com>
2023-05-12 14:40:53 +0200

    navbar fonctionnelle mais prb couleur icons

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
+import { View, StyleSheet, Text, Dimensions, ScrollView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const Stack = createNativeStackNavigator();
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+const Tab = createBottomTabNavigator();
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 //Importer les routes (=screens)
 import MenuScreen from './screens/MenuScreen';
@@ -11,12 +17,13 @@ import RegistrationScreen from './screens/RegistrationScreen';
 import WelcomeScreen from './screens/WelcomeScreen.js';
 import 'react-native-url-polyfill/auto';
 import CycleScreen from './screens/CycleScreen';
+import ProfileScreen from './screens/ProfileScreen';
 //import { Session } from '@supabase/supabase-js';
 
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Menu">
+            {/* <Stack.Navigator initialRouteName="Menu">
                 <Stack.Screen name="Menu" component={MenuScreen} options={{ title: 'Menu' }} />
                 <Stack.Screen name="Login" component={LoginScreen} options={{ title: 'Login' }} />
                 <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home' }} />
@@ -36,7 +43,91 @@ export default function App() {
                     options={{ title: 'Welcome' }}
                 />
                 <Stack.Screen name="Cycle" component={CycleScreen} options={{ title: 'Cycle' }} />
-            </Stack.Navigator>
+            </Stack.Navigator> */}
+            <Tab.Navigator
+                activeColor="#FEF6D9"
+                inactiveColor="black"
+                screenOptions={{
+                    tabBarActiveTintColor: '#FEF6D9',
+                    tabBarInactiveTintColor: 'black',
+
+                    tabBarStyle: { backgroundColor: '#86C8BC', height: 60, padding: 5 },
+                }}
+                style={styles.navigator}
+            >
+                <Tab.Screen
+                    name="Cycle"
+                    component={CycleScreen}
+                    options={{
+                        tabBarLabel: 'Cycle',
+                        tabBarLabelStyle: { padding: 5 },
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialCommunityIcons name="home" color={'black'} size={30} />
+                        ),
+                    }}
+                />
+                <Tab.Screen
+                    name="Calendar"
+                    component={CalendarScreen}
+                    options={{
+                        tabBarLabel: 'Calendrier',
+                        tabBarLabelStyle: { padding: 5 },
+                        tabBarIcon: ({ color, size }) => (
+                            <Ionicons name="calendar-outline" color={'black'} size={30} />
+                        ),
+                    }}
+                />
+                <Tab.Screen
+                    name="Login"
+                    component={LoginScreen}
+                    options={{
+                        tabBarLabel: 'Connexion',
+                        tabBarLabelStyle: { padding: 5 },
+                        tabBarIcon: ({ color, size }) => (
+                            <Ionicons name="log-in-outline" color={'black'} size={30} />
+                        ),
+                    }}
+                />
+                <Tab.Screen
+                    name="Registration"
+                    component={RegistrationScreen}
+                    options={{
+                        tabBarLabel: 'Inscription',
+                        tabBarLabelStyle: { padding: 5 },
+                        tabBarIcon: ({ color, size }) => (
+                            <Ionicons name="person-add-outline" color={'black'} size={30} />
+                        ),
+                    }}
+                />
+                <Tab.Screen
+                    name="Welcome"
+                    component={WelcomeScreen}
+                    options={{
+                        tabBarLabel: 'Bienvenue',
+                        tabBarLabelStyle: { padding: 5 },
+                        tabBarIcon: ({ color, size }) => (
+                            <Ionicons name="happy-outline" color={'black'} size={30} />
+                        ),
+                    }}
+                />
+                <Tab.Screen
+                    name="Mon compte"
+                    component={ProfileScreen}
+                    options={{
+                        tabBarLabel: 'Mon compte',
+                        tabBarLabelStyle: { padding: 5 },
+                        tabBarIcon: ({ color, size }) => (
+                            <Ionicons name="person-outline" color={'black'} size={30} />
+                        ),
+                    }}
+                />
+            </Tab.Navigator>
         </NavigationContainer>
     );
 }
+
+const styles = StyleSheet.create({
+    navigator: {
+        backgroundColor: '#86C8BC',
+    },
+});
