27d989d0c460f27cc01ad7136320f5a0f33dac4a
HortenseLaclaviere <114992888+HortenseLaclaviere@users.noreply.github.com>
2023-05-23 10:14:38 +0200

    ajout Iphone X

--- Components/BottomNavigation.js
+++ Components/BottomNavigation.js
@@ -11,93 +11,86 @@ import ProfileScreen from '../screens/ProfileScreen.js';
 const Tab = createBottomTabNavigator();
 
 const BottomNavigation = () => {
-  const deviceModel = Device.modelName;
-  console.log(deviceModel);
-  let tabBarIconSize;
-  let tabBarLabelStyle;
-  let tabBarStyle;
+    const deviceModel = Device.modelName;
+    console.log(deviceModel);
+    let tabBarIconSize;
+    let tabBarLabelStyle;
+    let tabBarStyle;
 
-  if (deviceModel.includes('iPhone 13 mini') || deviceModel.includes('iPhone 12') || deviceModel.includes('iPhone 14')) {
-    tabBarIconSize = 23;
-    tabBarLabelStyle = {
-      padding: 0,
-      marginBottom: -18,
-    };
-    tabBarStyle = {
-      height: 60,
-      padding: 0,
-      backgroundColor: '#86C8BC',
-    };
-  } else {
-    tabBarIconSize = 30;
-    tabBarLabelStyle = {
-      padding: 5,
-    };
-    tabBarStyle = {
-      height: 60,
-      padding: 5,
-      backgroundColor: '#86C8BC'
-    };
-  }
+    if (
+        deviceModel.includes('iPhone 13 mini') ||
+        deviceModel.includes('iPhone 12') ||
+        deviceModel.includes('iPhone 14') ||
+        deviceModel.includes('iPhone X')
+    ) {
+        tabBarIconSize = 23;
+        tabBarLabelStyle = {
+            padding: 0,
+            marginBottom: -18,
+        };
+        tabBarStyle = {
+            height: 60,
+            padding: 0,
+            backgroundColor: '#86C8BC',
+        };
+    } else {
+        tabBarIconSize = 30;
+        tabBarLabelStyle = {
+            padding: 5,
+        };
+        tabBarStyle = {
+            height: 60,
+            padding: 5,
+            backgroundColor: '#86C8BC',
+        };
+    }
 
-  return (
-    <Tab.Navigator
-      activeColor="#FEF6D9"
-      inactiveColor="black"
-      screenOptions={{
-        tabBarActiveTintColor: '#FEF6D9',
-        tabBarInactiveTintColor: 'black',
-        tabBarStyle,
-        headerShown: false,
-      }}
-    >
-      <Tab.Screen
-        name="Cycle"
-        component={CycleScreen}
-        options={{
-          tabBarLabel: 'Cycle',
-          tabBarLabelStyle,
-          tabBarIcon: ({ color }) => (
-            <Ionicons
-              name="refresh-outline"
-              color={color}
-              size={tabBarIconSize}
+    return (
+        <Tab.Navigator
+            activeColor="#FEF6D9"
+            inactiveColor="black"
+            screenOptions={{
+                tabBarActiveTintColor: '#FEF6D9',
+                tabBarInactiveTintColor: 'black',
+                tabBarStyle,
+                headerShown: false,
+            }}
+        >
+            <Tab.Screen
+                name="Cycle"
+                component={CycleScreen}
+                options={{
+                    tabBarLabel: 'Cycle',
+                    tabBarLabelStyle,
+                    tabBarIcon: ({ color }) => (
+                        <Ionicons name="refresh-outline" color={color} size={tabBarIconSize} />
+                    ),
+                }}
             />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Calendar"
-        component={CalendarScreen}
-        options={{
-          tabBarLabel: 'Calendrier',
-          tabBarLabelStyle,
-          tabBarIcon: ({ color }) => (
-            <Ionicons
-              name="calendar-outline"
-              color={color}
-              size={tabBarIconSize}
+            <Tab.Screen
+                name="Calendar"
+                component={CalendarScreen}
+                options={{
+                    tabBarLabel: 'Calendrier',
+                    tabBarLabelStyle,
+                    tabBarIcon: ({ color }) => (
+                        <Ionicons name="calendar-outline" color={color} size={tabBarIconSize} />
+                    ),
+                }}
             />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Profile"
-        component={ProfileScreen}
-        options={{
-          tabBarLabel: 'Profil',
-          tabBarLabelStyle,
-          tabBarIcon: ({ color }) => (
-            <Ionicons
-              name="person-outline"
-              color={color}
-              size={tabBarIconSize}
+            <Tab.Screen
+                name="Profile"
+                component={ProfileScreen}
+                options={{
+                    tabBarLabel: 'Profil',
+                    tabBarLabelStyle,
+                    tabBarIcon: ({ color }) => (
+                        <Ionicons name="person-outline" color={color} size={tabBarIconSize} />
+                    ),
+                }}
             />
-          ),
-        }}
-      />
-    </Tab.Navigator>
-  );
+        </Tab.Navigator>
+    );
 };
 
 export default BottomNavigation;
