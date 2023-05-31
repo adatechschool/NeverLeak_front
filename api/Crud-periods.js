921f565d744ece53c5bd9729d5fcfb841e70a90d
Doyen Audrey <audrey@metis-lab.com>
2023-05-31 15:32:04 +0200

    build-gestion periods dans le crud

--- api/Crud-periods.js
+++ api/Crud-periods.js
@@ -1,13 +1,16 @@
 import { supabase } from '../supabase';
+import { formatDistance, subDays } from 'date-fns';
+import compareAsc from 'date-fns/compareAsc';
 
 const getPeriodsDays = async (userId) => {
     const { data, error } = await supabase
         .from('multi-periods')
-        .select('start_date')
-        .eq('user_id', userId);
+        .select('end_date')
+        .eq('user_id', userId)
+        .order('end_date', { ascending: false });
 
     console.log('error read = ', error);
-    return data.map((e) => e.start_date);
+    return data;
 };
 
 function addDays(date, days) {
@@ -23,35 +26,45 @@ const postPeriodDay = async (userId, day) => {
     console.log('posts error =', error);
 };
 
-const updatePeriodDay = async (userId, day) => {
+const updatePeriodDay = async (newDate, day) => {
+    // const periodDays = await getPeriodsDays(userId);
+    // const id = periodDays.map((e) => e.id);
     const { data, error } = await supabase
         .from('multi-periods')
         .update({ end_date: day })
-        .eq('user_id', userId);
+        // .eq('user_id', userId)
+        .eq('end_date', newDate);
 
     console.log('posts error =', error);
 };
 
 const addPeriodDay = async (userId, day) => {
     const periodDays = await getPeriodsDays(userId);
+    const endDates = periodDays.map((e) => e.end_date);
     console.log({ periodDays });
-    if (periodDays.length < 1) {
+    if (endDates.length < 1) {
         postPeriodDay(userId, day);
     } else {
-        periodDays.map((e) => {
+        for (e of endDates) {
             const newDate = new Date(e);
             const result = addDays(newDate, 1);
-            const result2 = new Date(result);
-            console.log({ result2 });
-            const newDateDay = new Date(day);
-            console.log({ newDateDay });
-            if (newDateDay == result2) {
-                updatePeriodDay(userId, day);
+            console.log({ result });
+            // const result2 = new Date(result);
+            // console.log({ result2 });
+            // const newDateDay = new Date(day);
+            // console.log({ newDateDay });
+            const result3 = compareAsc(new Date(result), new Date(day));
+            console.log({ result3 });
+            if (result3 == 0) {
+                console.log({ e });
+                updatePeriodDay(e, day);
+                break;
             } else {
                 console.log('post');
                 postPeriodDay(userId, day);
+                break;
             }
-        });
+        }
     }
 };
 const deletePeriodDay = async (userId, day) => {
