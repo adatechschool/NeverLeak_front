685ec3dbb6f6eb29199e6b85ea74add6c173b110
hugorince <hugo.rince@gmail.com>
2023-06-29 16:23:28 +0200

    first commit trigger-function

--- api/Crud-periods.js
+++ api/Crud-periods.js
@@ -54,49 +54,57 @@ const updateStartDay = async (newDate, day) => {
 //changement de la fonction pour ajouter un jour au cycle : definition des const au bon format
 //pour les startday et les endday puis recherche du jour sélectionné dans le tableau des dates.
 //Si le jour existe dans le tableau, la fonction appelle updateStartDay ou updateEndDay
-const addPeriodDay = async (userId, day) => {
-    const periodDays = await getAllPeriods(userId);
-    //faire un tableau des endDates recupérées de la BDD
-    const endDates = periodDays.map((date) => date.end_date);
-    //faire un tableau des startDates récupérées de la BDD
-    const startDates = periodDays.map((date) => date.start_date);
-    //Ajouter ou retirer un jour aux dates de début et dates de fin puis les formatter
-    const daySelectedRemoved = addDays(new Date(day), -1);
-    const daySelectedAdded = addDays(new Date(day), 1);
-    const formattedEndDay = format(new Date(daySelectedRemoved), 'yyyy-MM-dd');
-    const formattedStartDay = format(new Date(daySelectedAdded), 'yyyy-MM-dd');
+// const addPeriodDay = async (userId, day) => {
+//     const periodDays = await getAllPeriods(userId);
+//     //faire un tableau des endDates recupérées de la BDD
+//     const endDates = periodDays.map((date) => date.end_date);
+//     //faire un tableau des startDates récupérées de la BDD
+//     const startDates = periodDays.map((date) => date.start_date);
+//     //Ajouter ou retirer un jour aux dates de début et dates de fin puis les formatter
+//     const daySelectedRemoved = addDays(new Date(day), -1);
+//     const daySelectedAdded = addDays(new Date(day), 1);
+//     const formattedEndDay = format(new Date(daySelectedRemoved), 'yyyy-MM-dd');
+//     const formattedStartDay = format(new Date(daySelectedAdded), 'yyyy-MM-dd');
 
-    //si pas de données dans la BDD, ajouter directement le jour.
-    if (endDates.length < 1) {
-        console.log('entré dans if longueur de endDate < 1');
-        postPeriodDay(userId, day);
-        return;
-    }
+//     //si pas de données dans la BDD, ajouter directement le jour.
+//     if (endDates.length < 1) {
+//         console.log('entré dans if longueur de endDate < 1');
+//         postPeriodDay(userId, day);
+//         return;
+//     }
 
-    //si le tableau des endDates contient le jour sélectionné -1, alors update le endDate
-    if (endDates.includes(formattedEndDay)) {
-        updateEndDay(formattedEndDay, day);
-        return;
-    }
+//     //si le tableau des endDates contient le jour sélectionné -1, alors update le endDate
+//     if (endDates.includes(formattedEndDay)) {
+//         updateEndDay(formattedEndDay, day);
+//         return;
+//     }
 
-    //si le tableau des startDay contient le jour sélectionné +1, alors update le startDate
-    if (startDates.includes(formattedStartDay)) {
-        updateStartDay(formattedStartDay, day);
-        return;
-    }
+//     //si le tableau des startDay contient le jour sélectionné +1, alors update le startDate
+//     if (startDates.includes(formattedStartDay)) {
+//         updateStartDay(formattedStartDay, day);
+//         return;
+//     }
 
-    //sinon poster le jour
-    postPeriodDay(userId, day);
-};
+//     //sinon poster le jour
+//     postPeriodDay(userId, day);
+// };
 
 const deletePeriodDay = async (userId, day) => {
     const { data, error } = await supabase
-        .from('multi-periods')
+        .from('days')
         .delete()
         .eq('user_id', userId)
         .eq('start_date', day);
 
     console.log('delete error = ', error);
+};
+
+const addPeriodDay = async (userId, day) => {
+    const { data, error } = await supabase
+        .from('days')
+        .insert([{ user_id: userId, period_day: day }], { returning: 'minimal' });
+
+    console.log('posts error =', error);
 };
 
 export { getPeriodsDays, addPeriodDay, deletePeriodDay, getAllPeriods, addDays };
--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -9,157 +9,144 @@ import { eachDayOfInterval, format } from 'date-fns';
 
 export default function CalendarScreen() {
     const { session, setSession } = useContext(SessionContext);
-    const [selectedDays, setSelectedDays] = useState({
-        selected: [],
-        marked: {},
-    });
-    const [nextPeriod, setNextPeriod] = useState({
-        selected: [],
-        marked: {},
-    });
-    const [isLoading, setIsLoading] = useState(false);
+    // const [selectedDays, setSelectedDays] = useState({
+    //     selected: [],
+    //     marked: {},
+    // });
+    // const [nextPeriod, setNextPeriod] = useState({
+    //     selected: [],
+    //     marked: {},
+    // });
+    // const [isLoading, setIsLoading] = useState(false);
 
-    const readPeriodsDays = async () => {
-        const periodsDaysList = await getAllPeriods(session.user.id);
+    // const readPeriodsDays = async () => {
+    //     const periodsDaysList = await getAllPeriods(session.user.id);
 
-        const eachDay = periodsDaysList.map((eachPeriod) => {
-            const startDay = new Date(eachPeriod.start_date);
-            const endDay = new Date(eachPeriod.end_date);
-            const array = eachDayOfInterval({
-                start: startDay,
-                end: endDay,
-            });
+    //     const eachDay = periodsDaysList.map((eachPeriod) => {
+    //         const startDay = new Date(eachPeriod.start_date);
+    //         const endDay = new Date(eachPeriod.end_date);
+    //         const array = eachDayOfInterval({
+    //             start: startDay,
+    //             end: endDay,
+    //         });
 
-            const arrayFormated = array.map((array) => {
-                const result = format(new Date(array), 'yyyy-MM-dd');
-                return result;
-            });
-            return arrayFormated;
-        });
+    //         const arrayFormated = array.map((array) => {
+    //             const result = format(new Date(array), 'yyyy-MM-dd');
+    //             return result;
+    //         });
+    //         return arrayFormated;
+    //     });
 
-        const dayList = eachDay.flat();
-        const dayListFormated = dayList.map((e) => {
-            const result = format(new Date(e), 'yyyy-MM-dd');
-            return result;
-        });
+    //     const dayList = eachDay.flat();
+    //     const dayListFormated = dayList.map((e) => {
+    //         const result = format(new Date(e), 'yyyy-MM-dd');
+    //         return result;
+    //     });
 
-        setSelectedDays(() => {
-            return {
-                selected: dayListFormated,
-                marked: markedPeriod(eachDay),
-            };
-        });
-        if (periodsDaysList.length > 0) {
-            setNextPeriod(() => {
-                return {
-                    selected: nextCycleCalculation(
-                        periodsDaysList[periodsDaysList.length - 1]['start_date']
-                    ),
-                    marked: markedNextPeriod(
-                        nextCycleCalculation(
-                            periodsDaysList[periodsDaysList.length - 1]['start_date']
-                        )
-                    ),
-                };
-            });
-        }
-        setIsLoading(false);
-    };
+    //     setSelectedDays(() => {
+    //         return {
+    //             selected: dayListFormated,
+    //             marked: markedPeriod(eachDay),
+    //         };
+    //     });
+    //     if (periodsDaysList.length > 0) {
+    //         setNextPeriod(() => {
+    //             return {
+    //                 selected: nextCycleCalculation(
+    //                     periodsDaysList[periodsDaysList.length - 1]['start_date']
+    //                 ),
+    //                 marked: markedNextPeriod(
+    //                     nextCycleCalculation(
+    //                         periodsDaysList[periodsDaysList.length - 1]['start_date']
+    //                     )
+    //                 ),
+    //             };
+    //         });
+    //     }
+    //     setIsLoading(false);
+    // };
 
     const handleOnPressDay = async (event) => {
-        setIsLoading(true);
-        const periodDay = event.dateString;
-
-        if (selectedDays.selected.includes(periodDay)) {
-            await deletePeriodDay(session.user.id, periodDay);
-            readPeriodsDays();
-        } else {
-            await addPeriodDay(session.user.id, periodDay);
-            readPeriodsDays();
-        }
+        console.log(event, typeof event);
+        const periodDay = new Date(event.timestamp);
+        await addPeriodDay(session.user.id, periodDay.toISOString());
     };
 
-    const markedPeriod = (eachDays) => {
-        //cette fonction stylise les jours selectionnés en paramètre
-        const markedDates = {};
-        if (eachDays.length > 0) {
-            eachDays.map((period) => {
-                period.map((day) => {
-                    if (day === period[0] && period.length > 1) {
-                        markedDates[day] = {
-                            startingDay: true,
-                            color: '#FF9A61',
-                            customTextStyle: {
-                                color: '#FFFFFF',
-                            },
-                        };
-                    } else if (day === period[period.length - 1] && period.length > 1) {
-                        markedDates[day] = {
-                            selected: true,
-                            endingDay: true,
-                            color: '#FF9A61',
-                        };
-                    } else if (day === period[0] && period.length === 1) {
-                        markedDates[day] = {
-                            //selected: true,
-                            disabled: true,
-                            startingDay: true,
-                            endingDay: true,
-                            color: '#FF9A61',
-                            customTextStyle: {
-                                color: '#FFFFFF',
-                            },
-                        };
-                    } else {
-                        markedDates[day] = {
-                            selected: true,
-                            color: '#FF9A61',
-                        };
-                    }
-                });
-            });
-        }
-        return markedDates;
-    };
+    // const markedPeriod = (eachDays) => {
+    //     //cette fonction stylise les jours selectionnés en paramètre
+    //     const markedDates = {};
+    //     if (eachDays.length > 0) {
+    //         eachDays.map((period) => {
+    //             period.map((day) => {
+    //                 if (day === period[0] && period.length > 1) {
+    //                     markedDates[day] = {
+    //                         startingDay: true,
+    //                         color: '#FF9A61',
+    //                         customTextStyle: {
+    //                             color: '#FFFFFF',
+    //                         },
+    //                     };
+    //                 } else if (day === period[period.length - 1] && period.length > 1) {
+    //                     markedDates[day] = {
+    //                         selected: true,
+    //                         endingDay: true,
+    //                         color: '#FF9A61',
+    //                     };
+    //                 } else if (day === period[0] && period.length === 1) {
+    //                     markedDates[day] = {
+    //                         //selected: true,
+    //                         disabled: true,
+    //                         startingDay: true,
+    //                         endingDay: true,
+    //                         color: '#FF9A61',
+    //                         customTextStyle: {
+    //                             color: '#FFFFFF',
+    //                         },
+    //                     };
+    //                 } else {
+    //                     markedDates[day] = {
+    //                         selected: true,
+    //                         color: '#FF9A61',
+    //                     };
+    //                 }
+    //             });
+    //         });
+    //     }
+    //     return markedDates;
+    // };
 
-    const markedNextPeriod = (days) => {
-        const markedPeriod = {};
-        days.sort().map((day) => {
-            if (day === days[0] && days.length > 1) {
-                markedPeriod[day] = {
-                    startingDay: true,
-                    color: '#F8CFB8',
-                    customTextStyle: {
-                        color: '#FFFFFF',
-                    },
-                };
-            } else if (day === days[days.length - 1] && days.length > 1) {
-                markedPeriod[day] = {
-                    selected: true,
-                    endingDay: true,
-                    color: '#F8CFB8',
-                };
-            } else {
-                markedPeriod[day] = {
-                    selected: true,
-                    color: '#F8CFB8',
-                };
-            }
-        });
-        return markedPeriod;
-    };
-
-    useEffect(() => {
-        setIsLoading(true);
-        readPeriodsDays();
-    }, []);
+    // const markedNextPeriod = (days) => {
+    //     const markedPeriod = {};
+    //     days.sort().map((day) => {
+    //         if (day === days[0] && days.length > 1) {
+    //             markedPeriod[day] = {
+    //                 startingDay: true,
+    //                 color: '#F8CFB8',
+    //                 customTextStyle: {
+    //                     color: '#FFFFFF',
+    //                 },
+    //             };
+    //         } else if (day === days[days.length - 1] && days.length > 1) {
+    //             markedPeriod[day] = {
+    //                 selected: true,
+    //                 endingDay: true,
+    //                 color: '#F8CFB8',
+    //             };
+    //         } else {
+    //             markedPeriod[day] = {
+    //                 selected: true,
+    //                 color: '#F8CFB8',
+    //             };
+    //         }
+    //     });
+    //     return markedPeriod;
+    // };
 
     return (
         <View style={styles.container}>
             <View style={{ height: 120 }} />
 
             <Spinner
-                visible={isLoading}
                 textContent={'chargement...'}
                 color={'#FF9A61'}
                 animation={'fade'}
@@ -180,7 +167,6 @@ export default function CalendarScreen() {
                 }}
                 onDayPress={handleOnPressDay}
                 markingType={'period'}
-                markedDates={{ ...selectedDays.marked, ...nextPeriod.marked }}
             />
         </View>
     );
